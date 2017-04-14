2bff49340cd65a0497493a9224eedca65886ff81
Ryan Orsinger <ryan.orsinger@gmail.com>
2017-04-14 16:54:17 -0500

    Add inheritance example classes

--- Person.php
+++ Person.php
@@ -1,41 +1,17 @@
 <?php
-class Person
-{
-    public $name;
-    public $lastName;
-    public $languages;
 
-    // for php 5.x the string type hint is invalid
-    public function __construct($name, $lastname)
+class Person {
+    public $firstName;
+    public $lastName;
+
+    public function __construct($firstName, $lastName)
     {
-        // We'll have a lesson about this later
-        /*if (!is_string($name)) {
-            throw new Exception('Names should be a string');
-        }*/
-        $this->name = $name;
-        $this->lastName = $lastname;
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
     }
 
     public function fullName()
     {
-        return $this->name . ' ' . $this->lastName;
+        return $this->firstName . ' ' . $this->lastName;
     }
 }
-
-/*$person = new Person('Zach', ['Java']); // Enforce rules when you construct an object
-
-echo $person->name, PHP_EOL;
-print_r($person->languages);*/
-
-/*var person = {
-    name: 'Jacob',
-    languages: ['php', 'css', 'js']
-};
-
-var ryan = Object.create(person);
-ryan.name = 'Ryan';
-ryan.languages = ['php', 'js', 'clojure'];
-
-var mario
-
-mario.languaes = null;*/
---
+++ Rectangle.php
@@ -0,0 +1,20 @@
+<?php 
+
+class Rectangle {
+	public $height;
+	public $width;
+
+	public function __construct($height, $width) {
+		$this->height = $height;	
+		$this->width = $width;
+	}
+
+	public function area() {
+		return $this->height * $this->width;
+	}
+
+	public function perimeter() {
+		return 2 * $this->height + 2 * $this->width;
+	}
+}
+
